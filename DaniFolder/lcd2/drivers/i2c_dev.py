c4f7d1f177c3a712b988df7e1be493d53a30473d
danivilardell <danivilardell@gmail.com>
2022-03-30 15:00:00 +0100

    update: special characters

--- DaniFolder/lcd2/drivers/i2c_dev.py
+++ DaniFolder/lcd2/drivers/i2c_dev.py
@@ -126,7 +126,18 @@ class Lcd:
     def lcd_write(self, cmd, mode=0):
         self.lcd_write_four_bits(mode | (cmd & 0xF0))
         self.lcd_write_four_bits(mode | ((cmd << 4) & 0xF0))
-
+    
+    def lcd_display_char(self, charNum, line):
+        if line == 1:
+            self.lcd_write(0x80)
+        if line == 2:
+            self.lcd_write(0xC0)
+        if line == 3:
+            self.lcd_write(0x94)
+        if line == 4:
+            self.lcd_write(0xD4)
+        self.lcd_write(charNum, Rs)            
+            
     # put string function
     def lcd_display_string(self, string, line):
         if line == 1:
--- DaniFolder/main.py
+++ DaniFolder/main.py
@@ -3,28 +3,19 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-from lcd.LCD import Rfid
+from lcd import Rfid
 
 class MyWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="LSD puzzle 2")
 
         self.rfid = Rfid()
-
-        #self.box = Gtk.Box(spacing=6)
-        #self.add(self.box)
-        #self.set_default_size(500, 60)
-
-        #self.entry = Gtk.Entry()
-        #self.box.pack_start(self.entry, True, True, 0)
         
         self.textview = Gtk.TextView()
         self.textview.set_size_request(300,200)
-        #self.box.pack_start(self.textview, True, True, 0)
 
         self.button1 = Gtk.Button(label="Write")
         self.button1.connect("clicked", self.on_button1_clicked)
-        #self.box.pack_start(self.button1, True, True, 0)
         
         self.grid = Gtk.Grid()
         self.grid.add(self.textview)
